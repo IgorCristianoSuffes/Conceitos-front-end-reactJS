3926e3236e622de52163c7eda7663020dcf8dbf4
Igor Cristiano Suffes <cristianosuqe@gmail.com>
2021-09-27 21:08:36 -0300

    conceitos-ReactJS

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+
+import backgroundImage from './assets/background.jpg';
+
+import './App.css';
+
+import Header from "./components/Header";
+
+function App() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
+
+    //asd asda sdasdasda s
+
+    async function handleAddProject() {
+        //projects.push(`Novo projeto ${Date.now()}`);
+
+        //setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Igor Cristiano Suffes"
+        }
+        );
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+
+    }
+
+    return (
+        <>
+            <Header title="Projects" />
+
+            <img width={300} src={backgroundImage}/>
+
+            <ul>
+                { projects.map(project => <li key={project.id} >{ project.title }</li>) }
+            </ul>
+
+            <button type="button" onClick= {handleAddProject} >Adicionar projeto</button>
+        </>
+    );
+}
+
+export default App;
